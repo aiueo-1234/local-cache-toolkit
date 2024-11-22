5d394af33587a4f7785f7f67be2bb46ee6af5b34
aiueo-1234 <130837816+aiueo-1234@users.noreply.github.com>
2024-11-22 11:03:46 +0900

    remove probably unused files

--- packages/attest/__tests__/index.test.ts
+++
@@ -1,6 +0,0 @@
-import {attest, attestProvenance} from '../src'
-
-it('exports functions', () => {
-  expect(attestProvenance).toBeInstanceOf(Function)
-  expect(attest).toBeInstanceOf(Function)
-})
